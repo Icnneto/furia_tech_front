0de3c938b86e3659ebc4ff82bada8afce4a7af08
Israel Nunes <israelnetonunes@gmail.com>
2025-04-30 09:39:38 -0300

    refact: logic to retrieve api key from backend

--- public/js/utils/countryStateApi.js
+++ public/js/utils/countryStateApi.js
@@ -1,27 +1,58 @@
-const requestOptions = {
-    method: 'GET',
-    headers: {"X-CSCAPI-KEY": "API_KEY"},
-    redirect: 'follow'
+async function getApiKey() {
+    const url = "http://localhost:3000/retrieve-api";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.apiKey;
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 export async function getCountryAPI() {
+    const apiKey = await getApiKey();
+    if (!apiKey) return [];
+
+    const requestOptions = {
+        method: 'GET',
+        headers: { "X-CSCAPI-KEY": apiKey },
+        redirect: 'follow'
+    };
+
     try {
         const response = await fetch("https://api.countrystatecity.in/v1/countries", requestOptions);
-        const countries = await response.json();
-
-        return countries;
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar países: ${response.status}`);
+        }
+        return await response.json();
     } catch (error) {
-        console.log(`Erro ao carregar países: ${error}`)
-    };
-};
+        console.error("Erro ao carregar países:", error.message);
+        return [];
+    }
+}
 
-export async function getStateAPI (country) {
+export async function getStateAPI(country) {
+    const apiKey = await getApiKey();
+    if (!apiKey) return [];
+
+    const requestOptions = {
+        method: 'GET',
+        headers: { "X-CSCAPI-KEY": apiKey },
+        redirect: 'follow'
+    };
+
     try {
         const response = await fetch(`https://api.countrystatecity.in/v1/countries/${country}/states`, requestOptions);
-        const states = await response.json();
-
-        return states;
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar estados: ${response.status}`);
+        }
+        return await response.json();
     } catch (error) {
-        console.log(`Erro ao carregar estados: ${error}`)
-    };
+        console.error("Erro ao carregar estados:", error.message);
+        return [];
+    }
 };
